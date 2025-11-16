5a4ddae4a6382d9eb7fafcd7637610ead3f4d2f5
Alex Dumouchelle <alex.dumouchelle484@gmail.com>
2025-11-16 10:39:48 -0500

    change style to material

--- init.lua
+++ init.lua
@@ -1,2 +1,17 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+require("mini.icons").setup({
+  file = {
+    patterns = {
+      -- Match files ending with .test.tsx
+      ["%.test%.ts$"] = {
+        glyph = "", -- the standard TSX icon
+        hl = "MiniIconsTestTsx",
+      },
+    },
+  },
+})
+
+-- Create the orange highlight group
+vim.api.nvim_set_hl(0, "MiniIconsTestTsx", { fg = "#FFA500" })
