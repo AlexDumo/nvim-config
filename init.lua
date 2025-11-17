9a733396d65a825d5209df0baa8dfacb4cf14f4a
Alex Dumouchelle <alex.dumouchelle484@gmail.com>
2025-11-17 09:10:08 -0500

    fix typescript lsp

--- init.lua
+++ init.lua
@@ -1,17 +1,44 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
-require("mini.icons").setup({
-  file = {
-    patterns = {
-      -- Match files ending with .test.tsx
-      ["%.test%.ts$"] = {
-        glyph = "", -- the standard TSX icon
-        hl = "MiniIconsTestTsx",
-      },
-    },
-  },
-})
+require("oil").setup()
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
--- Create the orange highlight group
-vim.api.nvim_set_hl(0, "MiniIconsTestTsx", { fg = "#FFA500" })
+local function publish_branch()
+  -- 1. Get the current branch name
+  local branch = vim.fn.system("git rev-parse --abbrev-ref HEAD"):gsub("\n", "")
+
+  if branch == "" then
+    vim.notify("Could not detect branch name.", vim.log.levels.ERROR)
+    return
+  end
+
+  -- 2. Run the push command
+  local cmd = "git push origin -u " .. branch
+  vim.notify("Running: " .. cmd)
+
+  vim.fn.jobstart(cmd, {
+    stdout_buffered = true,
+    stderr_buffered = true,
+    on_stdout = function(_, data)
+      if data then
+        vim.notify(table.concat(data, "\n"))
+      end
+    end,
+    on_stderr = function(_, data)
+      if data then
+        vim.notify(table.concat(data, "\n"), vim.log.levels.ERROR)
+      end
+    end,
+    on_exit = function(_, code)
+      if code == 0 then
+        vim.notify("Successfully pushed branch '" .. branch .. "'!", vim.log.levels.INFO)
+      else
+        vim.notify("git push failed (exit code " .. code .. ")", vim.log.levels.ERROR)
+      end
+    end,
+  })
+end
+
+-- Command: :PublishBranch
+vim.api.nvim_create_user_command("PublishBranch", publish_branch, {})
--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -32,17 +32,82 @@ if true then return {
   end,
 },
 {
-  "pmizio/typescript-tools.nvim",
-  dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-  opts = {},
-},
-{
     'marko-cerovac/material.nvim',
     -- priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function ()
       vim.cmd.colorscheme 'material'
       require('material.functions').change_style("deep ocean")
     end
+},
+  {
+  'stevearc/oil.nvim',
+  ---@module 'oil'
+  ---@type oil.SetupOpts
+  opts = {},
+  -- Optional dependencies
+  dependencies = { { "nvim-mini/mini.icons", opts = {} } },
+  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+  -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+  lazy = false,
+},
+{
+  "pmizio/typescript-tools.nvim",
+  dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+  opts = {},
+},
+  {
+  "neovim/nvim-lspconfig",
+  opts = {
+    -- make sure mason installs the server
+    servers = {
+      tsserver = {
+        enabled = false,
+      },
+      ts_ls = {
+        enabled = false,
+      },
+      vtsls = {
+        enabled = false,
+      },
+    },
+  },
+},
+  {
+  "folke/trouble.nvim",
+  opts = {}, -- for default options, refer to the configuration section for custom setup.
+  cmd = "Trouble",
+  keys = {
+    {
+      "<leader>xx",
+      "<cmd>Trouble diagnostics toggle<cr>",
+      desc = "Diagnostics (Trouble)",
+    },
+    {
+      "<leader>xX",
+      "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+      desc = "Buffer Diagnostics (Trouble)",
+    },
+    {
+      "<leader>cs",
+      "<cmd>Trouble symbols toggle focus=false<cr>",
+      desc = "Symbols (Trouble)",
+    },
+    {
+      "<leader>cl",
+      "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+      desc = "LSP Definitions / references / ... (Trouble)",
+    },
+    {
+      "<leader>xL",
+      "<cmd>Trouble loclist toggle<cr>",
+      desc = "Location List (Trouble)",
+    },
+    {
+      "<leader>xQ",
+      "<cmd>Trouble qflist toggle<cr>",
+      desc = "Quickfix List (Trouble)",
+    },
+  },
 }
 } end
 
